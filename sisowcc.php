b173ed51cc5787b4520b7ea0c252ba255992e3e4
sisow <sisow@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-08-13 09:59:47 +0000

    new version
    git-svn-id: https://plugins.svn.wordpress.org/sisow-for-woocommerce/tags/2.3.0@755554 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ sisowcc.php
@@ -0,0 +1,36 @@
+<?php
+/*
+Plugin Name: WooCommerce Sisow CreditCard
+Plugin URI: http://www.sisow.nl
+Description: The Sisow CreditCard Plugin for WooCommerce
+Version: 3.3.0
+Author: Sisow
+Author URI: http://www.sisow.nl
+*/
+
+add_action('plugins_loaded', 'woocommerce_creditcard_init', 0);
+
+function woocommerce_creditcard_init() 
+{			
+	if ( ! class_exists( 'WC_Payment_Gateway' ) ) { return; }
+	
+	require_once(WP_PLUGIN_DIR . "/" . plugin_basename( dirname(__FILE__)) . '/sisow/sisow.cls5.php');
+	require_once(WP_PLUGIN_DIR . "/" . plugin_basename( dirname(__FILE__)) . '/sisow/base.php');
+	
+	class WC_Sisow_creditcard extends SisowBase
+	{
+		function __construct() 
+		{
+			$this->_start('creditcard', 'Sisow CreditCard');
+		}
+	}
+	
+	add_filter('woocommerce_payment_gateways', 'add_sisow_creditcard_gateway' );
+	
+	function add_sisow_creditcard_gateway($methods)
+	{
+		$temp = 'WC_Sisow_creditcard';
+		$methods[] = $temp;
+		return $methods;
+	}
+}
---
+++ sisowwg.php
@@ -0,0 +1,36 @@
+<?php
+/*
+Plugin Name: WooCommerce Sisow
+Plugin URI: http://www.sisow.nl
+Description: The Sisow Webshop Giftcard Plugin for WooCommerce
+Version: 3.3.0
+Author: Sisow
+Author URI: http://www.sisow.nl
+*/
+
+add_action('plugins_loaded', 'woocommerce_webshop_init', 0);
+
+function woocommerce_webshop_init() 
+{			
+	if ( ! class_exists( 'WC_Payment_Gateway' ) ) { return; }
+	
+	require_once(WP_PLUGIN_DIR . "/" . plugin_basename( dirname(__FILE__)) . '/sisow/sisow.cls5.php');
+	require_once(WP_PLUGIN_DIR . "/" . plugin_basename( dirname(__FILE__)) . '/sisow/base.php');
+	
+	class WC_Sisow_webshop extends SisowBase
+	{
+		function __construct() 
+		{ 
+			$this->_start('webshop', 'Sisow Webshop Giftcard');
+		}
+	}
+	
+	add_filter('woocommerce_payment_gateways', 'add_sisow_webshop_gateway' );
+	
+	function add_sisow_webshop_gateway($methods)
+	{
+		$temp = 'WC_Sisow_webshop';
+		$methods[] = $temp;
+		return $methods;
+	}
+}
